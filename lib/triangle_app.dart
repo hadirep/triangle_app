f90542870a18e4d6f88265721782d744fbca3b24
Hadi Rahmah Esa Putra <hadiesarahma@gmail.com>
2023-10-25 08:10:05 +0700

    -

--- lib/triangle_app.dart
+++ lib/triangle_app.dart
@@ -2,14 +2,14 @@ detectTriangle(num sideA, num sideB, num sideC) {
   final sides = [sideA, sideB, sideC];
   sides.sort();
 
-  for (var side in sides) {
-    if(side < 1) {
+  sides.forEach((side) {
+    if (side < 1) {
       throw Exception();
     }
-  }
+  });
 
   if(sides[0] + sides[1] <= sides[2]) {
-    throw Exception('Inequal Triangle');
+    throw Exception('This is Inequal Triangle');
   }
 
   if(sides[0] == sides[1] && sides[0] == sides[2]) {
